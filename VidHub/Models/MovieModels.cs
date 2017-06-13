6d3c47fb240db206a52cee6de2267dfda8791ad0
honda010181 <vuha0218@gmail.com>
2017-06-12 22:56:42 -0500

    Added Movie Model

---
+++ VidHub/Models/MovieModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidHub.Models
+{
+    public class MovieModels
+    {
+
+    }
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string PosterPath { get; set; }
+
+        public Movie(string Title, string PosterPath)
+        {
+            this.Title = Title;
+            this.PosterPath = PosterPath;
+        }
+
+    }
+}
--- VidHub/Models/ReferencingModel.cs
+++ VidHub/Models/ReferencingModel.cs
@@ -18,28 +18,51 @@ namespace VidHub.Models
             filterContext.Controller.ViewBag.InvalidLoginInfo = HttpContext.Current.Session["InvalidLoginInfo"];
 
             filterContext.Controller.ViewBag.FeatureMovieList = GetFeatureMovieList();
+
+ 
         }
 
 
-        private List<string> GetFeatureMovieList()
+        private List<Movie> GetFeatureMovieList()
         {
-            List<string> movieList = new List<string>();
+            List<Movie> movieList = new List<Movie>();
+
+            Movie m1 = new Movie("Arival", "~/Images/Arival.jpg");
+            Movie m2 = new Movie("Chucky", "~/Images/Chucky.jpg");
+            Movie m3 = new Movie("Guardian Of Russia", "~/Images/GuardianOfRussia.jpg");
+            Movie m4 = new Movie("Moon Light", "~/Images/MoonLight.jpg");
+            Movie m5 = new Movie("Recovery", "~/Images/Recovery.jpg");
+            Movie m6 = new Movie("Sky Fall", "~/Images/SkyFall.jpg");
+            Movie m7 = new Movie("StarWars", "~/Images/StarWars.jpg");
+            Movie m8 = new Movie("The Magnificient 7", "~/Images/TheMagnificient7.jpg");
+            Movie m9 = new Movie("The Punisher", "~/Images/ThePunisher.jpg");
+            Movie m10 = new Movie("Under World", "~/Images/UnderWorld.jpg");
 
 
-            movieList.Add("~/Images/Arival.jpg");
-            movieList.Add("~/Images/Chucky.jpg");
+            movieList.Add(m1);
+            movieList.Add(m2);
+            movieList.Add(m3);
+            movieList.Add(m4);
+            movieList.Add(m5);
+            movieList.Add(m6);
+            movieList.Add(m7);
+            movieList.Add(m8);
+            movieList.Add(m9);
+            movieList.Add(m10);
+            //movieList.Add("~/Images/Arival.jpg");
+            //movieList.Add("~/Images/Chucky.jpg");
 
-            movieList.Add("~/Images/GuardianOfRussia.jpg");
-            movieList.Add("~/Images/MoonLight.jpg");
+            //movieList.Add("~/Images/GuardianOfRussia.jpg");
+            //movieList.Add("~/Images/MoonLight.jpg");
 
-            movieList.Add("~/Images/Recovery.jpg");
-            movieList.Add("~/Images/SkyFall.jpg");
+            //movieList.Add("~/Images/Recovery.jpg");
+            //movieList.Add("~/Images/SkyFall.jpg");
 
-            movieList.Add("~/Images/StarWars.jpg");
-            movieList.Add("~/Images/TheMagnificient7.jpg");
+            //movieList.Add("~/Images/StarWars.jpg");
+            //movieList.Add("~/Images/TheMagnificient7.jpg");
 
-            movieList.Add("~/Images/ThePunisher.jpg");
-            movieList.Add("~/Images/UnderWorld.jpg");
+            //movieList.Add("~/Images/ThePunisher.jpg");
+            //movieList.Add("~/Images/UnderWorld.jpg");
             return movieList;
         }
         
